e699fea98373362d421ed1d53da6190ff015fac1
Christian Hurtado <christian.hurtadosaez@gmail.com>
2021-01-04 15:59:31 +0100

    Base creada
    · Creación de escenas, botones y conexión entre ellas.
    · Añadida plantilla de Fishdex.

---
+++ ProyectoFinal/Assets/Scripts/ButtonsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonsController : MonoBehaviour
+{
+    //Carga la pantalla del circuito
+    public void Fishdex()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    //Carga la pantalla del menu
+    public void BackMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    //Carga la partida
+    public void Play()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    //Cierra el juego
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}

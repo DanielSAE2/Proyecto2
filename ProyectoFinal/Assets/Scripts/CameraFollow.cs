18d9de27a5a3c441438040fa8d2cbf8234debd0f
Christian Hurtado <christian.hurtadosaez@gmail.com>
2021-01-07 22:50:54 +0100

    Movimiento Principal
    · Creado el movimiento del barco.
    · Creado el movimiento del anzuelo.
    · La cámara sigue a lo que debe.
    · Puesto el fondo.

---
+++ ProyectoFinal/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player, bait;
+
+    private bool followPlayer;
+    private bool followBait;
+    // Start is called before the first frame update
+    void Start()
+    {
+        followPlayer = true;
+        followBait = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 fPlayer = player.transform.position;
+        fPlayer.z = transform.position.z;
+
+        Vector3 fBait = bait.transform.position;
+        fBait.z = transform.position.z;
+        fBait.x = transform.position.x;
+        /*if (player.transform.position.x >= 321f)
+        {
+            follow = true;
+        }*/ 
+    
+        if (followPlayer)
+        {  
+            if (!followBait)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, fPlayer, 10);    
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, fBait, 10);    
+            }
+        }
+
+        SwitchToBaitCamera();
+    }
+
+    private void SwitchToBaitCamera()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            followBait = !followBait;
+        }
+    }
+}
---
+++ ProyectoFinal/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float directionX;
+    private float directionY;
+    public GameObject bait;
+    public float speed = 10;
+    public bool switchController = false;
+
+    void Start()
+    {
+        bait.gameObject.SetActive (false); 
+    }
+
+    void Update()
+    {
+        if(!switchController)
+        {
+            directionX = Input.GetAxis("Horizontal");
+            transform.position = new Vector2(transform.position.x + directionX * speed * Time.deltaTime, transform.position.y);
+        }
+        else
+        {
+            if (bait.transform.localPosition.y <= -4f)
+            {
+
+                if(bait.transform.localPosition.x >= -35 && bait.transform.localPosition.x <= 35)
+                {
+                    directionX = Input.GetAxis("Horizontal");
+                    directionY = Input.GetAxis("Vertical");
+                    bait.transform.position = new Vector2(bait.transform.position.x + directionX * speed/3 * Time.deltaTime,
+                                                        bait.transform.position.y + directionY * speed * Time.deltaTime);
+                }
+                else if(bait.transform.localPosition.x < -35)
+                {
+                    bait.transform.localPosition = new Vector2(-35, bait.transform.localPosition.y);
+                }
+                else if(bait.transform.localPosition.x > 35)
+                {
+                    bait.transform.localPosition = new Vector2(35, bait.transform.localPosition.y);
+                }
+            }
+            else
+            {
+                bait.transform.localPosition = new Vector2(bait.transform.localPosition.x, -4f);
+
+            }
+        }
+        SwitchToBait();
+    }
+
+    private void SwitchToBait()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            switchController = !switchController;
+            bait.gameObject.SetActive (true);
+        }
+    }
+}

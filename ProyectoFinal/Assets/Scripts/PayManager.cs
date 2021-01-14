6f85f4b7972f7a6f6d712b03461b2b90082f381e
Christian Hurtado <christian.hurtadosaez@gmail.com>
2021-01-14 18:58:24 +0100

    Parallax & Muelle UI
    · Parallax con problemas.
    · UI del muelle.

--- ProyectoFinal/Assets/Scripts/PayManager.cs
+++ ProyectoFinal/Assets/Scripts/PayManager.cs
@@ -6,18 +6,16 @@ using UnityEngine.UI;
 public class PayManager : MonoBehaviour
 {
     public Text money;
-    public static int currency = 1000;
+    public static int currency = 500;
     public Text upgradeBoat;
-    public static int boatUpgrade = 50;
+    public static int boatUpgrade = 25;
     public Text upgradeBait;
     public static int baitUpgrade = 50;
 
     // Start is called before the first frame update
     void Start()
     {
-        money = GetComponent<Text>();
-        upgradeBoat = GetComponent<Text>();
-        upgradeBait = GetComponent<Text>();
+
     }
 
     // Update is called once per frame

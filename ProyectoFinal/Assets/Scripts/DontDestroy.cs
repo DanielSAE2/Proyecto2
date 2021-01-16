aec99401c494af4c5f0487735fbbf3d7deee764e
Christian Hurtado <christian.hurtadosaez@gmail.com>
2021-01-17 00:43:48 +0100

    Botones & Derrota
    · Rediseño de todos los botones.
    · Implementación de sonido en todos los botones.
    · Pantalla derrota funcional.

--- ProyectoFinal/Assets/Scripts/DontDestroy.cs
+++ ProyectoFinal/Assets/Scripts/DontDestroy.cs
@@ -9,7 +9,6 @@ public class DontDestroy : MonoBehaviour
     private void Awake()
     {
         GameObject[] musicObject = GameObject.FindGameObjectsWithTag("MenuMusic");
-        
         DontDestroyOnLoad(this.gameObject);
     }
 }
